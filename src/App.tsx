ca61d9d497b4e59acdca8aaf3578710865450f43
Luda <luda.tamb@mail.ru>
2022-01-07 20:06:18 +0300

    refactor: correct app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,19 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 import NewsItemPostPage from './pages/NewsItemPostPage';
 import HomePage from './pages/HomePage';
-
-import { getNews } from './services/getNews';
-import { updateNews } from './services/deleteNews';
-
 import { AppProps } from './types/types';
 
-const App: React.FC<AppProps> = ({ news, comments, buttonFunction }) => {
-  const dispatch = useDispatch();
-
-  const updateNewsFunction = () => {
-    dispatch(updateNews());
-    dispatch(getNews());
-  };
-
-  useEffect(() => {
-    dispatch(getNews());
-  });
-
-  setInterval(updateNewsFunction, 60000);
-
+const App: React.FC<AppProps> = ({ news, comments }) => {
   return (
-    <div className="App">
+    <>
       <Router>
         <Routes>
-          <Route
-            path="/"
-            element={<HomePage buttonFunction={buttonFunction} news={news} comments={comments} />}
-          />
+          <Route path="/" element={<HomePage news={news} comments={comments} />} />
           <Route path="/items/:id" element={<NewsItemPostPage news={news} comments={comments} />} />
         </Routes>
       </Router>
-    </div>
+    </>
   );
 };
 
