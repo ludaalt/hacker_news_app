f9972564035f1f8541ffe1cc75c38d782b0cd33e
Luda <luda.tamb@mail.ru>
2022-02-06 19:37:20 +0300

    refactor: change store accepting through hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useSelector, RootStateOrAny } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NewsItemPostPage from './pages/NewsItemPostPage';
 import HomePage from './pages/HomePage';
 import { AppProps } from './types/types';
 
-const App: React.FC<AppProps> = ({ news, comments }) => {
+const App: React.FC<AppProps> = () => {
+  const news = useSelector((state: RootStateOrAny) => state.news?.arrayNews);
+  const comments = useSelector((state: RootStateOrAny) => state.comments?.arrayComments);
+
   return (
     <>
       <Router>
--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider, connect, RootStateOrAny } from 'react-redux';
+import { Provider } from 'react-redux';
 
 import store from './store/store';
 import './index.css';
 import App from './App';
 
-const mapStateToProps = (state: RootStateOrAny) => {
-  return {
-    news: state.news.arrayNews,
-    comments: state.comments.arrayComments,
-  };
-};
-
-// const mapDispatchToProps = (dispatch: Dispatch<Action>) => {
-//   return {
-//     buttonFunction: () => {
-//       // dispatch(updateNews());
-//       // dispatch(getNews());
-//     },
-//   };
-// };
-
-const Container = connect(mapStateToProps, null)(App);
-
 ReactDOM.render(
   <Provider store={store}>
-    <Container />
+    <App />
   </Provider>,
   document.getElementById('root')
 );
