a76d17b2cbd66a3d510bb97e2ffb90e1e1016bd0
Luda <luda.tamb@mail.ru>
2022-03-04 14:30:30 +0300

    refactor: make ordered server responses

--- src/services/getNews.ts
+++ src/services/getNews.ts
@@ -4,16 +4,18 @@ import { Dispatch } from 'redux';
 import { Action } from '../types/action';
 
 const PAGES_COUNT = 4;
+const urls = [];
+
+for (let i = 1; i <= PAGES_COUNT; i += 1) {
+  urls.push(`https://api.hnpwa.com/v0/news/${i}.json`);
+}
+
+const requests = urls.map((url) => fetch(url));
 
 export const getNews = (): unknown => {
   return (dispatch: Dispatch<Action>) => {
-    for (let i = 1; i <= PAGES_COUNT; i += 1) {
-      fetch(`https://api.hnpwa.com/v0/news/${i}.json`)
-        .then((response) => response.json())
-        .then((json) => {
-          dispatch(addNewsAction(json));
-        })
-        .catch((error) => console.error('Error: ', error));
-    }
+    Promise.all(requests)
+      .then((responses) => Promise.all(responses.map((response) => response.json())))
+      .then((jsons) => jsons.forEach((json) => dispatch(addNewsAction(json))));
   };
 };
