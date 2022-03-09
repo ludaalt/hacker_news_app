245e5873a16666a71cdc570f8ffa7f32f957e5e4
Luda <luda.tamb@mail.ru>
2022-03-09 14:36:49 +0300

    refactor: correct comments component

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,41 +1,27 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { CommentsProps } from '../types/props';
+import { CommentsItemType } from '../types/types';
+
+import ShowCommentsContainer from './ShowCommentsContainer';
 
 const Comments: React.FC<CommentsProps> = ({ comments }) => {
-  useEffect(() => {
-    const nonDisplayCollection = document.querySelectorAll('.drop-container > ul');
-    for (let i = 0; i < nonDisplayCollection.length; i += 1) {
-      nonDisplayCollection[i].classList.add('non-display');
-    }
-  }, []);
-
-  const dropHandle = (event: React.MouseEvent<Element, MouseEvent>): void => {
-    const dropElement = event.target as HTMLElement;
-    dropElement.innerHTML = dropElement.innerHTML === '+' ? '-' : '+';
-
-    dropElement.nextElementSibling &&
-      dropElement.nextElementSibling.classList.toggle('non-display');
+  const makeCommentItem = (item: CommentsItemType) => {
+    return <div>{item.type === 'comment' && item.content}</div>;
   };
+
+  console.log(comments);
 
   return (
     <>
       {comments &&
-        comments.map((item, key: number) => (
-          <ul key={key}>
-            {item.comments && item.comments.length !== 0 ? (
-              <li className="drop-container">
-                {item.type === 'comment' && item.content && item.content.replace(/<p>/gi, '')}
-                <div className="drop" onClick={(event) => dropHandle(event)}>
-                  +
-                </div>
-                <Comments comments={item.comments} />
-              </li>
+        comments.comments?.map((item: CommentsItemType) => (
+          <div key={item.id}>
+            {item.comments && item.comments.length > 0 ? (
+              <ShowCommentsContainer item={item} />
             ) : (
-              <li>
-                {item.type === 'comment' && item.content && item.content.replace(/<p>/gi, '')}
-              </li>
+              <>{makeCommentItem(item)}</>
             )}
-          </ul>
+          </div>
         ))}
     </>
   );
