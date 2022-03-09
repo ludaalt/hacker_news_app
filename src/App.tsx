01ab50bcd56cb08fe15ebfba76d4ce7cc0ce821e
Luda <luda.tamb@mail.ru>
2022-03-09 10:55:47 +0300

    refactor: correct main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,54 @@
 import React from 'react';
-import { useSelector, RootStateOrAny } from 'react-redux';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import NewsItemPostPage from './pages/NewsItemPostPage';
+import Header from './components/Header';
 import HomePage from './pages/HomePage';
+
+import { getNews } from './services/getNews';
+import { deleteNews } from './services/deleteNews';
+
+import { getComments } from './services/getComments';
+import { deleteComments } from './services/deleteComments';
+
 import { AppProps } from './types/props';
 
 const App: React.FC<AppProps> = () => {
-  const news = useSelector((state: RootStateOrAny) => state.news?.arrayNews);
-  const comments = useSelector((state: RootStateOrAny) => state.comments?.arrayComments);
+  const dispatch = useDispatch();
+
+  const updateNews = () => {
+    dispatch(deleteNews());
+    dispatch(getNews());
+  };
+
+  const updateComments = (id: number) => {
+    dispatch(deleteComments());
+    dispatch(getComments(id));
+  };
 
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage news={news} comments={comments} />} />
-          <Route path="/items/:id" element={<NewsItemPostPage news={news} comments={comments} />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header page="home" updateNews={updateNews} />
+                <HomePage updateNews={updateNews} />
+              </>
+            }
+          />
+          <Route
+            path="/items/:id"
+            element={
+              <>
+                <Header page="post" updateComments={updateComments} />
+                <NewsItemPostPage updateComments={updateComments} />
+              </>
+            }
+          />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
     </>
