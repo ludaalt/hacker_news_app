ed5e5ae16db0fb5c814535fe6807c515d5edc8ad
Luda <luda.tamb@mail.ru>
2022-03-31 16:09:18 +0300

    refactor: change end of line sequence and delete files

--- src/components/UpdateCommentsButton.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Button from './Button';
-import { UpdateCommentsButtonProps } from '../types/props';
-
-const UpdateComentsButton: React.FC<UpdateCommentsButtonProps> = ({ title, updateComments }) => {
-  return <Button handleCommentsFunction={updateComments} title={title} />;
-};
-
-export default UpdateComentsButton;
