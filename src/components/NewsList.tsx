181cd509c02834dcda720784ccf588b9d2ed8aab
Luda <luda.tamb@mail.ru>
2022-03-31 15:48:44 +0300

    refactor: correct news list component

--- src/components/NewsList.tsx
+++ src/components/NewsList.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-
 import NewsItem from './NewsItem';
-import { AppProps } from '../types/props';
-import { NewsItemType } from '../types/types';
+import { NewsItemType, CommentsItemType } from '../types/types';
 
-const NewsContainer = styled.div`
-  margin: auto;
-  margin-top: 170px;
-  max-width: 50%;
-`;
+export interface NewsListProps {
+  news: Array<NewsItemType> | undefined;
+  comments: Array<CommentsItemType> | undefined;
+}
 
-const NewsList: React.FC<AppProps> = ({ news }) => {
+const NewsList: React.FC<NewsListProps> = ({ news }) => {
   return (
     <NewsContainer>
       {news &&
         news.map((item: NewsItemType, index: number) => {
           return (
             item && (
-              <div key={item.title}>
+              <div key={item.id}>
                 <Link to={`/items/${item.id}`}>
                   <NewsItem
                     title={item.title}
@@ -36,5 +33,11 @@ const NewsList: React.FC<AppProps> = ({ news }) => {
     </NewsContainer>
   );
 };
+
+const NewsContainer = styled.div`
+  margin: auto;
+  margin-top: 170px;
+  max-width: 50%;
+`;
 
 export default NewsList;
